437e9ad7a820866124ce306441eced169d2d6d27
kostvick <my.mail.kostya@gmail.com>
2023-01-11 17:26:35 +0200

    fix info card widget

--- lib/src/widgets/info_card.dart
+++ lib/src/widgets/info_card.dart
@@ -10,35 +10,48 @@ class InfoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: 157.0,
       padding: const EdgeInsets.all(20.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.0),
         color: ColorConstants.kBackgroundSecondary,
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(
-            child: Image.asset(
-              '/images/info_card_icon.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Text(
-                    heading,
-                    style: Theme.of(context).textTheme.headlineSmall!,
+          AspectRatio(
+            aspectRatio: 1 / 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    '/images/info_card_icon.png',
                   ),
                 ),
+              ),
+            ),
+          ),
+          // add spacing
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  heading,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+                const SizedBox(height: 10.0),
                 Expanded(
                   child: Text(
                     description,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(color: ColorConstants.kText, height: 1.4),
                   ),
                 ),
               ],
